419512b012cd9a8827668c0e43d4d7fd5448bbfc
HugoBuenoGarcia <hbuebo@alu.fpsanantonio.com>
2025-05-26 10:21:55 +0200

    Gestión de empleados refactorizada en una nueva clase Empleados con métodos para alta, aumento de salario y listado, y actualiza el main para usarla.

---
+++ Empleados.java
@@ -0,0 +1,25 @@
+public class Empleados {
+    public Empleado[] lista;
+
+    public Empleados(int cantidad) {
+        lista = new Empleado[cantidad];
+    }
+
+    public void altaEmpleado(int indice, String nombre, String cargo, double salario) {
+        lista[indice] = new Empleado(nombre, cargo, salario);
+    }
+
+    public void aumentarSalario(double porcentaje) {
+        for (Empleado empleado : lista) {
+            double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
+            empleado.setSalario(nuevoSalario);
+        }
+    }
+
+    public void mostrarEmpleados() {
+        System.out.println("Lista de Empleados:");
+        for (Empleado empleado : lista) {
+            System.out.println(empleado);
+        }
+    }
+}
